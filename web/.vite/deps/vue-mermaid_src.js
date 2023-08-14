75d3d9d537524985955c73514326eb8372872d8c
Marat Levit <mlevit@gmail.com>
2023-08-14 17:09:53 +1000

    Added vite deps

---
+++ web/.vite/deps/vue-mermaid_src.js
@@ -0,0 +1,12 @@
+// node_modules/vue-mermaid/src/index.js
+import VueMermaid from "/home/mlevit/git/echelon/web/node_modules/vue-mermaid/src/vue-mermaid.vue";
+function install(Vue) {
+  Vue.component("vue-mermaid", VueMermaid);
+}
+var src_default = {
+  install
+};
+export {
+  src_default as default
+};
+//# sourceMappingURL=vue-mermaid_src.js.map
